c7815cf9f247545b6d19d8239fc3a8aa4e28faaa
raioramalho <ramalho.sit@gmail.com>
2024-03-05 18:18:07 -0300

    iniciando meu app de pomodoro!

--- src/App.tsx
+++ src/App.tsx
@@ -20,20 +20,7 @@ function App() {
 
   return (
     <main id="main" className="flex flex-col">
-      <div id="navbar" className="p-2 m-2 flex flex-row justify-between items-center">
-        <h2>
-          Tauri + Vite + Shadcn
-        </h2>
-        <ThemeToggle/>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <h1>{greetMsg}</h1>
-        <form onSubmit={greet} className="flex flex-col">
-          <Input className="m-2 w-[200px]" placeholder="Digite seu nome:" onChange={(event) => setName(event.target.value)}/>
-          <Button className="m-2 w-[200px]" type="submit">Click-me</Button>
-        </form>
-      </div>
-
+      <h2>RaioPomo</h2> 
     </main>
   );
 }
