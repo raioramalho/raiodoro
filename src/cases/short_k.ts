624fe167d7e273cde147e5d1b086c6320a855e23
raioramalho <ramalho.sit@gmail.com>
2024-03-29 17:05:05 -0300

    I change command for control

--- src/cases/short_k.ts
+++ src/cases/short_k.ts
@@ -2,8 +2,8 @@ import handleStop from "@/actions/handle.stop";
 import { isRegistered, register } from "@tauri-apps/api/globalShortcut";
 
 export default async function ShortK(setIsRunning: any, setTimer: any) {
-    await isRegistered("CommandOrControl+K");
-    await register("CommandOrControl+K", () => {
+    await isRegistered("Control+K");
+    await register("Control+K", () => {
         handleStop(setIsRunning, setTimer);
     });
   }
--- src/cases/short_p.ts
+++ src/cases/short_p.ts
@@ -2,8 +2,8 @@ import handlePause from "@/actions/handle.pause";
 import { isRegistered, register } from "@tauri-apps/api/globalShortcut";
 
 export async function ShortP(setIsRunning: any, setTimer:any, timer: string) {
-    await isRegistered("CommandOrControl+P");
-    await register("CommandOrControl+P", () => {
+    await isRegistered("Control+P");
+    await register("Control+P", () => {
       handlePause(setIsRunning, setTimer, timer);
     });
   }
